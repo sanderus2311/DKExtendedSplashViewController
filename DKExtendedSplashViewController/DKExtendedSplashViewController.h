0cec603449ebc1adc5081218dda0666bb0d9f4dd
dkoncewicz <dawid.koncewicz@yahoo.com>
2018-02-04 14:29:07 +0100

    Initial Commit

---
+++ DKExtendedSplashViewController/DKExtendedSplashViewController.h
@@ -0,0 +1,19 @@
+//
+//  DKExtendedSplashViewController.h
+//  DKExtendedSplashViewController
+//
+//  Created by Dawid Koncewicz on 04/02/2018.
+//  Copyright © 2018 Dawid Koncewicz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKExtendedSplashViewController.
+FOUNDATION_EXPORT double DKExtendedSplashViewControllerVersionNumber;
+
+//! Project version string for DKExtendedSplashViewController.
+FOUNDATION_EXPORT const unsigned char DKExtendedSplashViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKExtendedSplashViewController/PublicHeader.h>
+
+
